02e53942b7b737ac224e234675602aea2270e657
Bilyana Ninova <bilyananinova@gmail.com>
2022-07-03 18:35:50 +0300

    add css variables

--- client/src/components/user/Login.js
+++ client/src/components/user/Login.js
@@ -38,7 +38,7 @@ function Login() {
                     <form className="login-form" onSubmit={handleSubmit}>
                         <label htmlFor="login-email">Email address<span className="required">*</span></label>
                         <input type="email" className="form-input" id="login-email" name="email" placeholder="ivan@mail.bg" />
-                        <label htmlFor="login-password">Password <span className="required">*</span></label>
+                        <label htmlFor="login-password">Password<span className="required">*</span></label>
                         <input type="password" className="form-input" id="login-password" name="password" placeholder="******" />
                         <button type="submit" className="login-button">Login</button>
                         <p>Don't have an account? <Link to="/register">Register</Link> now.</p>
--- client/src/components/user/Register.js
+++ client/src/components/user/Register.js
@@ -44,7 +44,7 @@ function Register() {
                         <input type="text" className="form-input" id="register-name" name="name" placeholder="Ivan Ivanov" />
                         <label htmlFor="register-email">Email address<span className="required">*</span></label>
                         <input type="email" className="form-input" id="register-email" name="email" placeholder="ivan@mail.bg" />
-                        <label htmlFor="register-password">Password <span className="required">*</span></label>
+                        <label htmlFor="register-password">Password<span className="required">*</span></label>
                         <input type="password" className="form-input" id="register-password" name="password" placeholder="******" />
                         <label htmlFor="register-repeat-password">Repeat Password <span className="required">*</span></label>
                         <input type="password" className="form-input" id="register-repeat-password" name="rePassword" placeholder="******" />
