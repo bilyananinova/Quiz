c264380103d9033cec8406a6c69a61ab24c24e20
Bilyana Ninova <bilyananinova@gmail.com>
2022-08-01 22:33:36 +0300

    logout function in error page

--- client/src/components/error/PageNotFound.js
+++ client/src/components/error/PageNotFound.js
@@ -1,16 +1,27 @@
 import './PageNotFound.css';
+import React from 'react';
 import { Link } from "react-router-dom";
 
-function PageNotFound() {
+import { logout } from '../../services/userServices';
+import { AuthContext } from '../../contexts/AuthContext';
 
+function PageNotFound() {
+    let { getUser } = React.useContext(AuthContext);
+
+    React.useEffect(() => {
+        logout()
+            .then(() => {
+                getUser();
+            })
+    }, []);
 
     return (
         <>
             <section className="error-wrapper">
-                    <h1 className="error-status">404</h1>
-                    <h4 className="error-heading">ERROR PAGE</h4>
-                    <p className="error-content">Don't worry! Just follow the link bellow to back home page</p>
-                    <Link className="error-link" to="/">Home</Link>
+                <h1 className="error-status">404</h1>
+                <h4 className="error-heading">ERROR PAGE</h4>
+                <p className="error-content">Don't worry! Just follow the link bellow to back home page</p>
+                <Link className="error-link" to="/">Home</Link>
             </section>
         </>
     )
